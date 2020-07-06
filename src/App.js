d11405ff098714bc1bb34affe3edda7a6e720f3d
dayat44 <miroj.hidayat20@gmail.com>
2020-07-06 08:13:44 +0700

    PUT data RES API pada REACT.JS

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import About from './Component/Fungsional/AboutComp';
 // import DetailComp from './Component/Fungsional/DetailComp';
 import ListComp from './Component/Fungsional/Class/ListComp';
 import TambahComp from './Component/Fungsional/Class/TambahComp';
+import EditComp from './Component/Fungsional/Class/EditComp';
 
 
 // import logo from './logo.svg';
@@ -22,6 +23,7 @@ const App = () => {
       <Route exact path="/about" component={About} />
       <Route exact path="/mahasiswa" component={ListComp} />
       <Route exact path="/mahasiswa/tambah" component={TambahComp} />
+      <Route exact path="/mahasiswa/edit" component={EditComp} />
       {/* <Route exact path="/detail/:id" component={DetailComp} /> */}
     </Switch>
 
